d845171483a5d653b1dec0e9305f9eb253daa058
Richard <rich.emartinez@hotmail.com>
2024-10-05 00:28:31 -0300

    borre todo por que me tiraba unos errores que no deberia

--- salvapos-frontend/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { LoginComponent } from './app/login/login.component'; // Asegúrate de que LoginComponent está correctamente importado
-
-bootstrapApplication(LoginComponent)
-  .catch(err => console.error(err));
