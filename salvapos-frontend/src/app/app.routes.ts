f0207cdff8f9ad15c92bc0d4a3f5986629e1ef39
Richard <rich.emartinez@hotmail.com>
2024-10-06 18:38:24 -0300

    modifique app.routes

--- salvapos-frontend/src/app/app.routes.ts
+++ salvapos-frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+//import { HomeComponent } from './home/home.component';//
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent }, // Ruta para login
+  { path: 'registro', component: RegisterComponent }, // Ruta para registro
+ // { path: 'home', component: HomeComponent }, // Ruta para home
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirigir al login por defecto
+  { path: '**', redirectTo: 'login' } // Redirigir al login si la ruta no existe
+];
