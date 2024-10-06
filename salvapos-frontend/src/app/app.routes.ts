fe775658e47ebac217d05469f9d66d28896e85d2
Richard <rich.emartinez@hotmail.com>
2024-10-06 20:27:12 -0300

    modifique routers y puse inventario

--- salvapos-frontend/src/app/app.routes.ts
+++ salvapos-frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 //import { HomeComponent } from './home/home.component';//
 import { RegisterComponent } from './register/register.component';
+import { InventarioComponent } from './inventario/inventario.component';
 
 export const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent }, // Ruta para login
-  { path: 'registro', component: RegisterComponent }, // Ruta para registro
+    { path: 'login', component: LoginComponent }, // Ruta para login
+    { path: 'registro', component: RegisterComponent }, // Ruta para registro
  // { path: 'home', component: HomeComponent }, // Ruta para home
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirigir al login por defecto
-  { path: '**', redirectTo: 'login' } // Redirigir al login si la ruta no existe
+    {path: 'inventario', component: InventarioComponent }, 
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirigir al login por defecto
+    { path: '**', redirectTo: 'login' } // Redirigir al login si la ruta no existe
 ];
