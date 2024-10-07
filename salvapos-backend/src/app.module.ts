83c0431e51acf97422b7a3e2054675903dd29131
Pablo Avila <pablo.avila2171@gmail.com>
2024-10-07 00:26:57 -0300

    mysql nest

--- salvapos-backend/src/app.module.ts
+++ salvapos-backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'salvapos_user',
+      password: '123456',
+      database: 'salvapos_db',
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    }),
+
+    AuthModule,
+
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- salvapos-backend/src/main.ts
+++ salvapos-backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Authorizer Docs')
+    .setDescription('The Authorizer API description')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui.html', app, document);
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
