012c12e509b617bf10c79624915c099e5af7cb1b
Richard <rich.emartinez@hotmail.com>
2024-10-06 21:56:40 -0300

    arregle el login

---
+++ salvapos-frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; // Asegúrate de que el componente de Login está importado
+import { RegisterComponent } from './register/register.component'; // Asegúrate de que el componente de Registro está importado
+//import { InventarioComponent } from './inventario/inventario.component'; // Asegúrate de que el componente de Inventario está importado
+
+// Definimos las rutas de la aplicación
+export const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },  // Ruta para la página de Login
+  { path: 'registro', component: RegisterComponent },  // Ruta para la página de Registro
+  //{ path: 'inventario', component: InventarioComponent },  // Ruta para la página de Inventario
+  { path: '', redirectTo: 'login', pathMatch: 'full' },  // Redirige a Login por defecto
+  { path: '**', redirectTo: 'login' }  // Redirige a Login si la ruta no existe
+];
