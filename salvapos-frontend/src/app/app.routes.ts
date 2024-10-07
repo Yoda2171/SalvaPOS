51dc239acbb03b008adc17226fef6b3cdc876287
Richard <rich.emartinez@hotmail.com>
2024-10-06 22:54:27 -0300

    Borrado unas cosas del app.routers

--- salvapos-frontend/src/app/app.routes.ts
+++ salvapos-frontend/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'; // Asegúrate de que el componente de Login está importado
-import { RegisterComponent } from './register/register.component'; // Asegúrate de que el componente de Registro está importado
-import { InventarioComponent } from './inventario/inventario.component'; // Asegúrate de que el componente de Inventario está importado
+import { LoginComponent } from './login/login.component'; 
+import { RegisterComponent } from './register/register.component'; 
+import { InventarioComponent } from './inventario/inventario.component'; 
+
 
 // Definimos las rutas de la aplicación
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },  // Ruta para la página de Login
-  { path: 'register', component: RegisterComponent },  // Ruta para la página de Registro
-  { path: 'inventario', component: InventarioComponent },  // Ruta para la página de Inventario
-  { path: '', redirectTo: 'login', pathMatch: 'full' },  // Redirige a Login por defecto
-  { path: '**', redirectTo: 'login' }  // Redirige a Login si la ruta no existe
+  { path: 'login', component: LoginComponent },  
+  { path: 'register', component: RegisterComponent }, 
+  { path: 'inventario', component: InventarioComponent },  
+  { path: '', redirectTo: 'login', pathMatch: 'full' },  
+  { path: '**', redirectTo: 'login' }  
 ];
