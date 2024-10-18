49d3aa43160b4a90a97787f94bea734c81305a73
Pablo Avila <pablo.avila2171@gmail.com>
2024-10-18 15:55:26 -0300

    arreglo de rutas

--- salvapos-frontend/src/app/dashboard/pages/inventario/inventario.component.ts
+++ salvapos-frontend/src/app/dashboard/pages/inventario/inventario.component.ts
@@ -1,12 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import {
-  ActivatedRoute,
-  Router,
-  RouterModule,
-  NavigationEnd,
-} from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inventario',
@@ -16,31 +10,6 @@ import { filter } from 'rxjs/operators';
   styleUrls: ['./inventario.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class InventarioComponent implements OnInit {
-  isAddOrEditProductRoute: boolean = false;
-  constructor(
-    private readonly router: Router,
-    private readonly route: ActivatedRoute
-  ) {}
-
-  ngOnInit(): void {
-    // Escuchar los cambios de la ruta activa
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        this.checkCurrentRoute();
-      });
-
-    // También ejecutar la lógica cuando se carga inicialmente
-    this.checkCurrentRoute();
-  }
-
+export default class InventarioComponent {
   // Método para verificar si la ruta actual es addproduct o editproduct
-  checkCurrentRoute() {
-    const currentRoute = this.route.snapshot.firstChild?.routeConfig?.path;
-    // Convertir la expresión en un booleano explícito
-    this.isAddOrEditProductRoute =
-      currentRoute === 'addproduct' ||
-      (currentRoute?.startsWith('editproduct') ?? false);
-  }
 }
